374737fdacf7715d1973de3419adf5a7e4f08500
EgorDiachkow <komrad.diachckow2014@yandex.ru>
2021-02-28 01:47:49 +0400

    Added a btn setting and save. added tooltip in save brn

--- src/renderer/components/containers/RigthBar.jsx
+++ src/renderer/components/containers/RigthBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { FiSettings } from 'react-icons/fi';
+import { FaSave } from 'react-icons/fa';
 import { Route } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip';
 import classes from './MenuBar.module.css';
 import StatistickContainer from './main/statistick/StatistickContainer.jsx';
 import CalculatorContainer from './main/Calculator/CalculatorContainer.jsx';
@@ -7,9 +10,12 @@ import CalculatorContainer from './main/Calculator/CalculatorContainer.jsx';
 export default function rightMenuBar() {
   return (
     <div className={classes.containerrigtht}>
+      <div className={classes.iconSetting}><FiSettings size="18px" color="#fff" /></div>
+      <div className={classes.iconSave} data-tip="Сохраните платёж"><FaSave size="18px" color="#fff" /></div>
       <Route exact path="/">1</Route>
       <Route path="/raschet"><CalculatorContainer /></Route>
       <Route path="/state"><StatistickContainer /></Route>
+      <ReactTooltip place="bottom" effect="solid" />
     </div>
   );
 }
