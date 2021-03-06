559ccaded9f89a34680bc0446214f9901b8a6d5d
EgorDiachkow <komrad.diachckow2014@yandex.ru>
2021-03-06 20:40:56 +0400

    wrote the logic for calculating the price

--- src/renderer/components/containers/main/Calculator/CalculatorContainer.jsx
+++ src/renderer/components/containers/main/Calculator/CalculatorContainer.jsx
@@ -7,46 +7,49 @@ import Setting from '../../../../entity/Setting.js';
 import classes from './Calculator.module.css';
 
 export default function CalculatorContainer() {
-  const { register, handleSubmit, watch, errors, reset } = useForm();
+  const { register, handleSubmit } = useForm();
   // eslint-disable-next-line no-use-before-define
   const onSubmit = (result) => getTotalValue(result);
   const [data, setData] = useState(false);
   const [total, setTotal] = useState(0);
-  const rate = [
-    {
-      diapazon: [0, 90],
-      value: 0.4626,
-    },
-    {
-      diapazon: [91, 150],
-      value: 0.6668,
-    },
-    {
-      diapazon: [151, 1000],
-      value: 0.8388,
-    },
-  ];
+  const [totalRate, setTotalRate] = useState();
 
-  function getTotalValue(result) {
+  function getEnergyValue(result) {
     const kvtValue = +result['Киловатт'];
     let totalValue = 0;
     let currentIndex = 0;
 
-    rate.forEach((item, index) => {
-      if (index === 2) rate[2].diapazon[1] = kvtValue;
-      const currentDiapazon = item.diapazon;
-      const currentRate = item.value;
-
+    data[0].rate.forEach((item, index) => {
       let currentValue = 0;
 
-      for (let i = currentIndex; i < currentDiapazon[1] && currentIndex < kvtValue; i++) {
-        currentValue += currentRate;
+      if (index === 2) data[0].rate[2].diapazon[1] = kvtValue;
+
+      for (let i = currentIndex; i < item.diapazon[1] && currentIndex < kvtValue; i++) {
+        currentValue += item.value;
         currentIndex++;
-        console.log(currentIndex, 'currentIndex');
       }
       totalValue += currentValue;
     });
+    setTotalRate(Math.ceil(totalValue));
+    return Math.ceil(totalValue);
+  }
+
+  function getTotalValue(result) {
+    const listItem = Object.entries(result);
+    let totalValue = 0;
+
+    listItem.forEach((item) => {
+      if (item[0] !== 'Энергия' && item[0] !== 'Киловатт') {
+        totalValue += +item[1];
+      }
+    });
+    totalValue += getEnergyValue(result);
     setTotal(totalValue);
+  }
+
+  function reserValue() {
+    setTotalRate('');
+    setTotal('');
   }
 
   useEffect(() => {
@@ -75,26 +78,28 @@ export default function CalculatorContainer() {
                   className={classes.inputItem}
                   id={product.name}
                   name={product.name}
-                  defaultValue={product.rate}
-                  ref={register({ max: 5000, min: 0, maxLength: 100 })}
+                  disabled={product.disable}
+                  defaultValue={product.name === 'Энергия' ? totalRate : product.rate}
+                  ref={register({ max: 5000, min: 1, maxLength: 100 })}
                 />
                 {product.prefix === 'руб.' ? <BiRuble size="18px" color="#414241" /> : <HiOutlineLightBulb size="18px" color="#414241" />}
               </div>
             ))
           ) : (
-            <h1>Загрузка данных</h1>
-          )}
+              <h1>Загрузка данных</h1>
+            )}
         </div>
-        {total > 0 ? (<div className={classes.totalValue}>{`${Math.ceil(total)} Рублей`}</div>
+        {total > 0 ? (<div className={classes.totalValue}>{`${total} Рублей`}</div>
         ) : (
-          <></>
-        )}
+            <></>
+          )}
         <div className={classes.actionContainer}>
           <input className={`${classes.btn}`} value="рассчитать" type="submit" />
           <input
             className={`${classes.btn} ${classes.reset}`}
             type="reset"
             value="Сбросить"
+            onClick={reserValue}
           />
         </div>
       </form>
