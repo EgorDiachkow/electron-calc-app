85d3c3a97fd5e725628dd6b7ec675e6e222e1fe4
EgorDiachkow <komrad.diachckow2014@yandex.ru>
2021-03-09 23:44:53 +0400

    added ruble icon

--- src/renderer/components/containers/main/statistick/StatistickContainer.jsx
+++ src/renderer/components/containers/main/statistick/StatistickContainer.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { LineChart } from 'react-chartkick';
 import 'chart.js';
-import ModelStatistick from '../../../../model/ModelStatistic.js';
+import { BiRuble } from 'react-icons/bi';
 import classes from './Statistick.module.css';
 
 export default function StatistickContainer() {
@@ -21,6 +21,7 @@ export default function StatistickContainer() {
     getTotalYear(statistickItems);
     setStateDate(statistickItems.reverse());
   }
+
   function getTotalYear(date) {
     const data = new Date().getFullYear();
     const filteredItems = date.filter((item) => item[0].indexOf(data) !== -1);
@@ -49,11 +50,17 @@ export default function StatistickContainer() {
       <div className={classes.containerTotal}>
         <div className={classes.totalItem}>
           Всего за текущий год:
-          <div className={classes.totalValue}>{`${yearTotal} р.`}</div>
+          <div className={classes.totalValue}>
+            {yearTotal}
+            <BiRuble size="16px" color="#414241" />
+          </div>
         </div>
         <div className={classes.totalItem}>
           За всё время:
-          <div className={classes.totalValue}>{`${allTotal} р.`}</div>
+          <div className={classes.totalValue}>
+            {allTotal}
+            <BiRuble size="16px" color="#414241" />
+          </div>
         </div>
       </div>
       <div className={classes.items}>
@@ -62,10 +69,14 @@ export default function StatistickContainer() {
           <span>Оплата</span>
         </div>
         {stateDate ? (
-          stateDate.map((elem) => (
-            <div key={elem[1]} className={`${classes.item}`}>
+          stateDate.map((elem, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={`${elem[1]} ${index}`} className={`${classes.item}`}>
               <span>{elem[0]}</span>
-              <span>{`${elem[1]} р.`}</span>
+              <span>
+                {elem[1]}
+                <BiRuble size="16px" color="#414241" />
+              </span>
             </div>
           ))
         ) : (
