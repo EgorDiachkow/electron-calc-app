7dbec447b5302fe27a04af4c509cde18db34603a
EgorDiachkow <komrad.diachckow2014@yandex.ru>
2021-03-14 23:39:17 +0400

    changed name component

--- src/renderer/components/containers/LeftBar.jsx
+++ src/renderer/components/containers/LeftBar.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import MenuContainer from './menu/MenuContainer.jsx';
 import Logo from '../logo/Logo.jsx';
 import classes from './MenuBar.module.css';
-import UserProfile from '../profile/UserProfileContainer.jsx';
+import { UserProfileContainer } from '../profile/UserProfileContainer.jsx';
 
 export default function LeftMenuBar() {
   return (
     <div className={classes.container}>
       <Logo />
       <MenuContainer />
-      <UserProfile />
+      <UserProfileContainer />
     </div>
   );
 }
