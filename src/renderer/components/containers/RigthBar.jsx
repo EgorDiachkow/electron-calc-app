048a7ce9f8e4f8e80b121ba84d457c90691c62cd
EgorDiachkow <komrad.diachckow2014@yandex.ru>
2021-03-14 23:39:53 +0400

    imported the module via destructuring

--- src/renderer/components/containers/RigthBar.jsx
+++ src/renderer/components/containers/RigthBar.jsx
@@ -3,9 +3,9 @@ import { FiSettings } from 'react-icons/fi';
 import { Route, NavLink } from 'react-router-dom';
 import SavePopUp from '../modal/SavePopUp.jsx';
 import classes from './MenuBar.module.css';
-import StatistickContainer from './main/statistick/StatistickContainer.jsx';
+import { StatistickContainer } from './main/statistick/StatistickContainer.jsx';
 import { CalculatorContainer } from './main/Calculator/CalculatorContainer.jsx';
-import MainSetting from './setting/MainSetting.jsx';
+import { MainSetting } from './setting/MainSetting.jsx';
 
 export default function rightMenuBar() {
   return (
