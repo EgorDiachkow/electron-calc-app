536d4c1a8a842308a7adefb4baf09647474dd615
EgorDiachkow <komrad.diachckow2014@yandex.ru>
2021-03-14 23:34:25 +0400

    imported component store

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,2 +1,6 @@
 export * from './openPopUpStore.js';
 export * from './mainDataStore.js';
+export * from './statisticStore.js';
+export * from './settingStore.js';
+export * from './userProfileStore.js';
+export * from './savePopUpStore.js';
